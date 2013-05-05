c0f7ab39fafd70a52c71427f6c9ed66f4a9b049e
Kanoshiko <raidelet@gmail.com>
2013-05-05 11:42:41 +0200

    Tri par init et init global

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,14 @@
 
 angular.module('myApp.controllers', []).
         controller('CtrCombat', ['$rootScope', '$scope', function($rootScope, $scope) {
+        $scope.triParInit = function(pnj) {
+            return -Number(pnj.initJet);
+        };
 
-
-    }]);
+        $scope.lancerInits = function() {
+            for (var i in $rootScope.pnjs) {
+                $rootScope.pnjs[i].lancerInit();
+            }
+        };
+    }
+]);
